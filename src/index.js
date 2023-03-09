d4dc25c4d69d9e3a99a14ab0b7a603c394b1d324
jagritx <jagritkamra1@gmail.com>
2023-03-09 11:46:31 +0530

    Production Changes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import App from "./App";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+axios.defaults.baseURL = "https://mix-api-production.up.railway.app/api";
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
